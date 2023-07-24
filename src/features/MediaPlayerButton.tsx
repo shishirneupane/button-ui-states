5d92e2c64b2dcbf2c00cdc64ac3f70c747978828
Shishir Neupane <shishir@outside.tech>
2023-07-25 03:35:48 +0545

    "media player" completed

--- src/features/MediaPlayerButton.tsx
+++ src/features/MediaPlayerButton.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import Button from "../components/Button";
 
 const MediaPlayerButton = () => {
-  return <Button className="btn-player">PLAY</Button>;
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayer = () => setIsPlaying((isPlaying) => !isPlaying);
+
+  return (
+    <>
+      <Button className="btn-player" onClick={togglePlayer}>
+        <p>+</p>
+        <p>{isPlaying ? "PAUSE" : "PLAY"}</p>
+        <p className="player-duration">0:35 / 5:23</p>
+      </Button>
+    </>
+  );
 };
 
 export default MediaPlayerButton;
